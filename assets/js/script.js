1b766ebe90ae1ab1318e452e0963d82603173bc4
Cesar Gonzalez <gcesar13@yahoo.com>
2022-06-02 00:47:28 -0600

    worked on style of the page and adding bootstrap grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
+let tasks = document.querySelector(".tasks");
 
 // Setting current date, time and year to the header
 
 var CurrentTime = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").append(CurrentTime);
 console.log(CurrentTime);
+
+
+//save tasks in local storage so they stay when page is reloaded
+
+tasks.value= localStorage.getItem("tasks");
+
+tasks.addEventListener("keyup", event => {
+    localStorage.setItem("tasks", tasks.value);
+});
 
