69884aee00a1e83715f8b0516cdcaba075743f72
Cesar Gonzalez <gcesar13@yahoo.com>
2022-06-04 12:10:50 -0600

    got save to work on page refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
-var saveBtn = $(".saveBtn");
-
 // Setting current date, time and year to the header
 
 var CurrentTime = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").append(CurrentTime);
 
+
 //save tasks in local storage so they stay when page is reloaded
+
+
+var saveBtn = $(".saveBtn");
 
 saveBtn.on("click", function () {
 
-   var time = $(this).siblings(".hour").text();
-   var tasks = $(this).siblings(".tasks").val();
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-    localStorage.setItem(time,tasks);
-    console.log("save")
-    tasks.value=localStorage.getItem(time,tasks);
-});
-
+        // Save text in local storage
+        localStorage.setItem(time, text);
+})
 
 //sets color blocks for past, present, and future
 
@@ -39,7 +39,15 @@ function timeTracker() {
 
   };
 
+  $("#8 .description").val(localStorage.getItem("8"));
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
 
   timeTracker();
-
-  
